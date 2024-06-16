16584a319424bed2e0b724bb1ec82b0b67758f51
macpro <r.man.abi@hotmail.com>
2024-06-16 02:38:58 -0400

    finished

--- src/app/checking/seat-choosing/seat-choosing.component.ts
+++ src/app/checking/seat-choosing/seat-choosing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,13 +6,36 @@ import { Router } from '@angular/router';
   templateUrl: './seat-choosing.component.html',
   styleUrl: './seat-choosing.component.scss'
 })
-export class SeatChoosingComponent {
+export class SeatChoosingComponent implements AfterViewInit,AfterViewChecked ,OnInit{
+  @ViewChild('seatmapWrapperDesktop') content: ElementRef<HTMLInputElement> | undefined
   constructor(private router: Router) {
 
+  }
+  ngOnInit(): void {
+    
+  }
+  ngAfterViewChecked(): void {
+    
+  }
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+    // this.scrollBottomToUp()
+      
+    }, 3000);
   }
   backToProhibited() {
     this.router.navigateByUrl('check-in/prohibited-object')
   }
   continue() { }
+
+  scrollBottomToUp = () => {
+    try {
+      if (this.content?.nativeElement) {
+        // this.content.nativeElement.scrollTop = this.content?.nativeElement.scrollHeight;
+        debugger
+        this.content.nativeElement.scrollTo({ left: this.content?.nativeElement.scrollWidth, behavior: 'smooth' })
+      }
+    } catch (err) { }
+  }
 
 }
